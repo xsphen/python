8126aa44417fe951fae678e847a3d5031a44d434
wxiangbo <wxiangbo@gmail.com>
2016-03-13 17:06:49 +0800

    add file

---
+++ java/src/Abc.java
@@ -0,0 +1,13 @@
+/**
+ * java
+ * PACKAGE_NAME
+ * Created by Administrator on 22:13.
+ */
+public class Abc {
+    public static void main(String[] args){
+        String s = "wxiangbo";
+
+        System.out.println("hello world!");
+        System.out.println(s);
+    }
+}
---
+++ java/src/Abc0313.java
@@ -0,0 +1,15 @@
+/**
+ * java
+ * PACKAGE_NAME
+ * Created by Administrator on 17:02.
+ */
+public class Abc0313 {
+    static int j = 456;
+    public static void main(String args[]){
+        int i = 123;
+        boolean k = true;
+        System.out.println(i);
+        System.out.println(j);
+        System.out.println(k);
+    }
+}
