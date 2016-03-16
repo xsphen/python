54031f151d369de44d57d7ee1e650dc649b0493a
wxiangbo <wxiangbo@gmail.com>
2016-03-16 15:40:14 +0800

    add file

---
+++ python/helloworld.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# @Author: wxiangbo
+# @Date:   2016-03-16 15:37:09
+# @Last Modified by:   wxiangbo
+# @Last Modified time: 2016-03-16 15:38:06
+#
+#   理论上来说第一个文件应该是helloworld
+#
+print("Hello World!");
