2c0a3935a6f6eb306febfedc36ba902a7db6ed02
wxiangbo <wxiangbo@gmail.com>
2016-03-17 18:07:34 +0800

    add file

---
+++ python/lesson_1.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# @Author: wxiangbo
+# @Date:   2016-03-16 16:02:33
+# @Last Modified by:   wxiangbo
+# @Last Modified time: 2016-03-16 16:20:02
+print('abc') # python3 输出命令
+
+print(id(3)) # 查看对象内存地址
---
+++ python/lesson_2.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# @Author: wxiangbo
+# @Date:   2016-03-17 15:39:10
+# @Last Modified by:   wxiangbo
+# @Last Modified time: 2016-03-17 16:21:20
+print(2 + 5)
+
+print(5 - 2)
+print(10 / 2)
+print(5 * 2)
+print(10 / 5 + 1)
+print(4.0 + 2.0)
+
+print(type(1))      # int
+print(type(1.0))    # float
+print(abs(-12))     # 12
+
+print(round(5.4))   # 四舍五入
+print(pow(2,3))     # 幂次方
+
